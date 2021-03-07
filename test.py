0022f9c7a7630cc17737bc025a21ae43c6b9d14e
Bo Chen <bochen1993cs@gmail.com>
2021-03-07 15:01:19 -0600

    test run

--- test.py
+++ test.py
@@ -278,15 +278,159 @@ def test(data,
         maps[c] = ap[i]
     return (mp, mr, map50, map, *(loss.cpu() / len(dataloader)).tolist()), maps, t
 
+def get_model(opt):
+    device = select_device(opt.device, batch_size=opt.batch_size)
+    # Load model
+    model = attempt_load(opt.weights, map_location=device)  # load FP32 model
+    # Half
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+    if half:
+        model.half()
+    model.eval()
+    print(device.type)
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, opt.imgsz, opt.imgsz).to(device).type_as(next(model.parameters())))  # run once
+    return model
 
-if __name__ == '__main__':
+def get_dataloader(opt,model):
+    device = select_device(opt.device, batch_size=opt.batch_size)
+    # Load model
+    gs = max(int(model.stride.max()), 32)  # grid size (max stride)
+    imgsz = check_img_size(opt.img_size, s=gs)  # check img_size
+
+    # Configure
+    with open(opt.data) as f:
+        data = yaml.load(f, Loader=yaml.SafeLoader)  # model dict
+    check_dataset(data)  # check
+    nc =  int(data['nc'])  # number of classes
+    
+    # no augment
+    dataloader = create_dataloader(data[opt.task] , imgsz, opt.batch_size, gs, opt, pad=0.5, rect=True,
+                                   prefix=colorstr(opt.task + ':'))[0]
+    return dataloader,nc
+
+def runmodel(opt,model,dataloader,nc,batch_idx_range,TF=None,C_param=None):
+    device = select_device(opt.device, batch_size=opt.batch_size)
+    
+    iouv = torch.linspace(0.5, 0.95, 10).to(device)  # iou vector for mAP@0.5:0.95
+    niou = iouv.numel()
+    half = device.type != 'cpu'
+
+    seen = 0
+    names = {k: v for k, v in enumerate(model.names if hasattr(model, 'names') else model.module.names)}
+    coco91class = coco80_to_coco91_class()
+    s = ('%20s' + '%12s' * 6) % ('Class', 'Images', 'Targets', 'P', 'R', 'mAP@.5', 'mAP@.5:.95')
+    p, r, f1, mp, mr, map50, map, t0, t1 = 0., 0., 0., 0., 0., 0., 0., 0., 0.
+    stats, ap, ap_class = [], [], []
+    for batch_i, (img, targets, paths, shapes) in enumerate(tqdm(dataloader, desc=s)):
+        if batch_i<batch_idx_range[0]:continue
+        elif batch_i>=batch_idx_range[1]:break
+        # perform transformation
+        tf_imgs = None
+        img = img[:,::-1,:,:] #rgb->bgr
+        for th_img in img:
+            np_img = th_img.permute(1,2,0).numpy()
+            tf_img = TF.trasform(image=np_img, C_param=C_param)
+            tf_img = torch.from_numpy(tf_img).float().permute(2,0,1).unsqueeze(0)
+            if tf_imgs is None:
+                tf_imgs = tf_img
+            else:
+                tf_imgs = torch.cat((tf_imgs,tf_img),0)
+        img = tf_imgs[:,::-1,:,:]
+        # end transformation
+        img = img.to(device, non_blocking=True)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        targets = targets.to(device)
+        nb, _, height, width = img.shape  # batch size, channels, height, width
+
+        with torch.no_grad():
+            # Run model
+            t = time_synchronized()
+            out, train_out = model(img, augment=opt.augment)  # inference and training outputs
+            t0 += time_synchronized() - t
+
+            # Run NMS
+            targets[:, 2:] *= torch.Tensor([width, height, width, height]).to(device)  # to pixels
+            lb = [targets[targets[:, 0] == i, 1:] for i in range(nb)] if opt.save_hybrid else []  # for autolabelling
+            t = time_synchronized()
+            out = non_max_suppression(out, conf_thres=opt.conf_thres, iou_thres=opt.iou_thres, labels=lb, multi_label=True)
+            t1 += time_synchronized() - t
+
+        # Statistics per image
+        for si, pred in enumerate(out):
+            labels = targets[targets[:, 0] == si, 1:]
+            nl = len(labels)
+            tcls = labels[:, 0].tolist() if nl else []  # target class
+            seen += 1
+
+            if len(pred) == 0:
+                if nl:
+                    stats.append((torch.zeros(0, niou, dtype=torch.bool), torch.Tensor(), torch.Tensor(), tcls))
+                continue
+
+            # Predictions
+            predn = pred.clone()
+            scale_coords(img[si].shape[1:], predn[:, :4], shapes[si][0], shapes[si][1])  # native-space pred
+
+            # Assign all predictions as incorrect
+            correct = torch.zeros(pred.shape[0], niou, dtype=torch.bool, device=device)
+            if nl:
+                detected = []  # target indices
+                tcls_tensor = labels[:, 0]
+
+                # target boxes
+                tbox = xywh2xyxy(labels[:, 1:5])
+                scale_coords(img[si].shape[1:], tbox, shapes[si][0], shapes[si][1])  # native-space labels
+
+                # Per target class
+                for cls in torch.unique(tcls_tensor):
+                    ti = (cls == tcls_tensor).nonzero(as_tuple=False).view(-1)  # prediction indices
+                    pi = (cls == pred[:, 5]).nonzero(as_tuple=False).view(-1)  # target indices
+
+                    # Search for detections
+                    if pi.shape[0]:
+                        # Prediction to target ious
+                        ious, i = box_iou(predn[pi, :4], tbox[ti]).max(1)  # best ious, indices
+
+                        # Append detections
+                        detected_set = set()
+                        for j in (ious > iouv[0]).nonzero(as_tuple=False):
+                            d = ti[i[j]]  # detected target
+                            if d.item() not in detected_set:
+                                detected_set.add(d.item())
+                                detected.append(d)
+                                correct[pi[j]] = ious[j] > iouv  # iou_thres is 1xn
+                                if len(detected) == nl:  # all targets already located in image
+                                    break
+
+            # Append statistics (correct, conf, pcls, tcls)
+            stats.append((correct.cpu(), pred[:, 4].cpu(), pred[:, 5].cpu(), tcls))
+
+    # Compute statistics
+    stats = [np.concatenate(x, 0) for x in zip(*stats)]  # to numpy
+    if len(stats) and stats[0].any():
+        p, r, ap, f1, ap_class = ap_per_class(*stats, plot=False, names=names)
+        ap50, ap = ap[:, 0], ap.mean(1)  # AP@0.5, AP@0.5:0.95
+        mp, mr, map50, map = p.mean(), r.mean(), ap50.mean(), ap.mean()
+        nt = np.bincount(stats[3].astype(np.int64), minlength=nc)  # number of targets per class
+    else:
+        nt = torch.zeros(1)
+
+    # Print results
+    pf = '%20s' + '%12.3g' * 6  # print format
+    print(pf % ('all', seen, nt.sum(), mp, mr, map50, map))
+
+    return map50
+
+def setup_opt():
     parser = argparse.ArgumentParser(prog='test.py')
     parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-    parser.add_argument('--data', type=str, default='data/coco128.yaml', help='*.data path')
+    parser.add_argument('--data', type=str, default='data/coco.yaml', help='*.data path')
     parser.add_argument('--batch-size', type=int, default=32, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.001, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.6, help='IOU threshold for NMS')
+    parser.add_argument('--iou-thres', type=float, default=0.65, help='IOU threshold for NMS')
     parser.add_argument('--task', default='val', help="'val', 'test', 'study'")
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--single-cls', action='store_true', help='treat as single-class dataset')
@@ -302,8 +446,32 @@ if __name__ == '__main__':
     opt = parser.parse_args()
     opt.save_json |= opt.data.endswith('coco.yaml')
     opt.data = check_file(opt.data)  # check file
-    print(opt)
     check_requirements()
+    return opt
+
+class Simulator:
+    def __init__(self):
+        self.opt = setup_opt()
+        self.model = get_model(self.opt)
+        self.dataloader,self.nc = get_dataloader(self.opt,self.model)
+        self.batch_per_point = 4
+        self.point_per_sim = len(self.dataloader)//self.batch_per_point
+
+    def get_one_point(self, index, TF=None, C_param=None):
+        assert(index<self.point_per_sim)
+        # create batch idxes for simulation
+        total_batches = len(self.dataloader)
+        batch_idx_range = (index*self.batch_per_point,(index+1)*self.batch_per_point)
+        map50 = runmodel(self.opt,self.model,self.dataloader,self.nc,batch_idx_range,TF,C_param)
+        return map50
+
+if __name__ == '__main__':
+    sim = Simulator()
+    r = sim.get_one_point(0)
+    print(r,sim.point_per_sim)
+    # opt = setup_opt()
+    # print(opt)
+    exit(0)
 
     if opt.task in ['val', 'test']:  # run normally
         test(opt.data,
