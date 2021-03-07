7d8ced71c2c4e7136e06d9cd853d5b7e3316c3b3
Bo Chen <bochen1993cs@gmail.com>
2021-03-07 16:36:18 -0600

    typo

--- test.py
+++ test.py
@@ -330,7 +330,7 @@ def runmodel(opt,model,dataloader,nc,batch_idx_range,TF=None,C_param=None):
             tf_imgs = None
             for th_img in img:
                 np_img = th_img.permute(1,2,0).numpy()
-                tf_img = TF.trasform(image=np_img[:,:,::-1], C_param=C_param)
+                tf_img = TF.transform(image=np_img[:,:,::-1], C_param=C_param)
                 tf_img = torch.from_numpy(tf_img[:,:,::-1]).float().permute(2,0,1).unsqueeze(0)
                 if tf_imgs is None:
                     tf_imgs = tf_img
