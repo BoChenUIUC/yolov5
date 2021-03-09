d0e3f7b9b7beb31e1242f4106c6b8b90c21fc521
Bo Chen <bochen1993cs@gmail.com>
2021-03-08 18:35:35 -0600

    0,3697

--- train_compression.py
+++ train_compression.py
@@ -182,7 +182,7 @@ def RL_train(net):
 	sim = Simulator()
 	cgen = C_Generator()
 	num_cfg = 100 # number of cfgs to be explored
-	selected_ranges = [10,50,100,500,1000,2000]
+	selected_ranges = [500,1000,1500,2000,2500,3000,3697]
 	print('Num batches:',num_cfg,sim.num_batches)
 
 	TF = Transformer('compression')
