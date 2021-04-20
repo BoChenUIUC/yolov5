ad87efc4da3551437c6abe140d09278a21440085
Bo Chen <bochen1993cs@gmail.com>
2021-04-20 09:20:19 -0500

    test

--- app.py
+++ app.py
@@ -277,9 +277,9 @@ def deepcod_main():
             # backprop
             loss = orthorgonal_regularizer(gen_model.sample.weight,0.0001,half)
             loss += criterion_mse(img,recon)
-            for origin_feat,recon_feat in zip(origin_features,recon_features):
-                if origin_feat is None:continue
-                loss += criterion_mse(origin_feat,recon_feat)
+            # for origin_feat,recon_feat in zip(origin_features,recon_features):
+            #     if origin_feat is None:continue
+            #     loss += criterion_mse(origin_feat,recon_feat)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
