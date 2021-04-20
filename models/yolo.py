60d7a3ece5bebe8b3d098b1ee0bc2f9551e8dc91
Bo Chen <bochen1993cs@gmail.com>
2021-04-20 13:00:49 -0500

    test

--- models/yolo.py
+++ models/yolo.py
@@ -46,6 +46,7 @@ class Detect(nn.Module):
 
             if not self.training:  # inference
                 if self.grid[i].shape[2:4] != x[i].shape[2:4]:
+                    print(x[i].device)
                     self.grid[i] = self._make_grid(nx, ny).to(x[i].device)
 
                 y = x[i].sigmoid()
