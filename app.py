02ee49fd97825588136e4b50cc31f67a9e2a8bbc
Bo Chen <bochen1993cs@gmail.com>
2021-05-11 06:49:33 -0500

    test

--- app.py
+++ app.py
@@ -244,7 +244,7 @@ def evaluate_config(gamma1=0.0001,gamma2=0.0001):
     thresh = torch.FloatTensor([0.5])
     if half: thresh = thresh.cuda()
     print(gamma1,gamma2,thresh)
-    for epoch in range(1,51):
+    for epoch in range(1,7):
         # train
         gen_model.train()
         if half:
@@ -549,7 +549,7 @@ def deepcod_main():
 
     thresh = torch.FloatTensor([0.5])
     if half: thresh = thresh.cuda()
-    for epoch in range(1,51):
+    for epoch in range(1,7):
         rlcr = AverageMeter()
         # train
         gen_model.train()
