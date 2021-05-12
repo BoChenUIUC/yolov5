aad60133149e5c369d071f6b1714fe229791bf07
Bo Chen <bochen1993cs@gmail.com>
2021-05-12 18:17:45 -0500

    test 0,0.0001

--- app.py
+++ app.py
@@ -549,7 +549,7 @@ def deepcod_main():
 
     thresh = torch.FloatTensor([0.5])
     if half: thresh = thresh.cuda()
-    for epoch in range(1,8):
+    for epoch in range(1,11):
         rlcr = AverageMeter()
         # train
         gen_model.train()
