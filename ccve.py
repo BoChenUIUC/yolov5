9fdff72ff85c1df22dc694afb125dc92c9dba430
Bo Chen <bochen1993cs@gmail.com>
2021-05-15 09:03:07 -0500

    shared

--- ccve.py
+++ ccve.py
@@ -593,7 +593,7 @@ def test():
 	# deepcod_validate()
 	from app import evaluate_config
 	# cfgs = [[0.01, 0.1]]
-	cfgs = [[0.01, 1]]
+	cfgs = [[0.01, 0.0001]]
 	cfg_file = open('cfg.log', "w", 1)
 	acc_file = open('acc.log', "w", 1)
 	cr_file = open('cr.log', "w", 1)
