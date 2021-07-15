65f7ca7032ed20f0125e9dc921b6a9379455f5ca
Bo Chen <bochen1993cs@gmail.com>
2021-07-15 12:24:49 -0500

    test bw

--- sender.py
+++ sender.py
@@ -19,7 +19,16 @@ def deepcod_send():
 	ADDR = ("130.126.136.154",8000)
 	client.connect(ADDR)
 
-	crs = [0.0039,0.0049,0.024,0.036,0.166]
+	crs = [0.173,0.312,0.236,0.251,0.16,0.15,0.03,0.173,0.036,0.025]
+
+	for cr in crs:
+		size = int(32*32*3*cr)
+		data = bytearray(os.urandom(size))
+		data = struct.pack(">L", len(data))+data
+		for i in range(10):
+			time_str = str(datetime.datetime.now())
+			print(cr,size,len(data))
+			client.send(str.encode(time_str)+data)
 
 	for cr in crs:
 		size = int(224*224*3*cr)
