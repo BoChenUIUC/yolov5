57e9af4bdc101c3535e6546183bbdd4fad99561a
Bo Chen <bochen1993cs@gmail.com>
2021-07-15 13:32:05 -0500

    test send

--- sender.py
+++ sender.py
@@ -20,7 +20,7 @@ def deepcod_send():
 	ADDR = ("130.126.136.154",8848)
 	client.connect(ADDR)
 
-	crs = [0.173]#,0.312,0.236,0.251,0.16,0.15,0.03,0.173,0.036,0.025]
+	crs = [0.173,0.312,0.236,0.251,0.16,0.15,0.03,0.173,0.036,0.025]
 
 	rdata = b""
 	for cr in crs:
@@ -28,6 +28,7 @@ def deepcod_send():
 		sdata = bytearray(os.urandom(size))
 		msg_size = len(sdata)
 		sdata = struct.pack(">L", len(sdata))+sdata
+		avg_t = 0
 		for i in range(10):
 			t1 = datetime.datetime.now()
 			client.send(sdata)
@@ -37,16 +38,28 @@ def deepcod_send():
 				rdata += tmp_str
 			rdata = rdata[msg_size:]
 			rtt = (datetime.datetime.now() - t1).total_seconds()
-			print(i,rtt)
+			avg_t += rtt/2
+		avg_t /= 10
+		print(cr,avg_t)
 
-	# for cr in crs:
-	# 	size = int(224*224*3*cr)
-	# 	data = bytearray(os.urandom(size))
-	# 	data = struct.pack(">L", len(data))+data
-	# 	for i in range(10):
-	# 		time_str = str(datetime.datetime.now())
-	# 		# print(cr,size,len(data))
-	# 		client.send(str.encode(time_str)+data)
+	for cr in crs:
+		size = int(224*224*3*cr)
+		sdata = bytearray(os.urandom(size))
+		msg_size = len(sdata)
+		sdata = struct.pack(">L", len(sdata))+sdata
+		avg_t = 0
+		for i in range(10):
+			t1 = datetime.datetime.now()
+			client.send(sdata)
+			while len(rdata) < msg_size:
+				tmp_str = client.recv(4096)
+				if not tmp_str:break
+				rdata += tmp_str
+			rdata = rdata[msg_size:]
+			rtt = (datetime.datetime.now() - t1).total_seconds()
+			avg_t += rtt/2
+		avg_t /= 10
+		print(cr,avg_t)
 	client.close()
 
 if __name__ == "__main__":
