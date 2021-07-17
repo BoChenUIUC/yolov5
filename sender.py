078cf452cdcc26403d43e88e3c4ecd6283d09d8b
Bo Chen <bochen1993cs@gmail.com>
2021-07-17 12:36:30 -0500

    test

--- sender.py
+++ sender.py
@@ -21,7 +21,7 @@ def deepcod_send():
 	# ADDR = ("127.0.0.1",8848)
 	ADDR = ("130.126.136.154",8000)
 	client.connect(ADDR)
-	delta = datetime.timedelta(seconds=0.060)
+	delta = datetime.timedelta(seconds=0.0)
 	crs = [0.15/32,0.03,0.173/32,0.036,0.025]
 	for cr in crs:
 		size = int(224*224*3*cr)
