2b46944bac16298a6cbba20e418b11ec3fbc92b0
Bo Chen <bochen1993cs@gmail.com>
2021-07-17 12:40:06 -0500

    test

--- sender.py
+++ sender.py
@@ -19,9 +19,8 @@ def deepcod_send():
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	payload_size = struct.calcsize(">L")
 	# ADDR = ("127.0.0.1",8848)
-	ADDR = ("130.126.136.154",8000)
+	ADDR = ("130.126.136.154",8848)
 	client.connect(ADDR)
-	delta = datetime.timedelta(seconds=0.0)
 	crs = [0.15/32,0.03,0.173/32,0.036,0.025]
 	for cr in crs:
 		size = int(224*224*3*cr)
@@ -30,7 +29,7 @@ def deepcod_send():
 		print(cr,size,len(data))
 		for i in range(10):
 			time.sleep(0.5)
-			time_str = str(datetime.datetime.now()-delta)
+			time_str = str(datetime.datetime.now())
 			client.send(str.encode(time_str)+data)
 
 	crs = [0.173/32,0.312,0.236/32,0.251,0.16]
@@ -41,7 +40,7 @@ def deepcod_send():
 		print(cr,size,len(data))
 		for i in range(10):
 			time.sleep(0.5)
-			time_str = str(datetime.datetime.now()-delta)
+			time_str = str(datetime.datetime.now())
 			client.send(str.encode(time_str)+data)
 	client.close()
 
